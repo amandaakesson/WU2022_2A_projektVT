1dea88a0304dfd53a584fce55ce2631a27530e5d
amandaakesson <94969010+amandaakesson@users.noreply.github.com>
2022-05-27 18:29:25 +0200

    har arbetat färdigt med hemsidan

--- js/app.js
+++ js/app.js
@@ -9,3 +9,23 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+//Knappen
+var mybutton = document.getElementById("myBtn");
+
+// När användaren skrollar ner 20px från toppen visas knappen
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// När användaren klickar på knappen skrollas man högst upp på sidan
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
